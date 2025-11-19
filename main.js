a91469027117e00a8e17f6d70064cb4d8a1dbc2f
John Pedraza <johnpedraza32@gmail.com>
2025-11-19 01:13:48 -0800

    retab

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function increaseFontSize() {
-  txt = document.getElementById('a');
-  style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
-  currentSize = parseFloat(style);
-  txt.style.fontSize = (currentSize + 10) + 'px';
+    txt = document.getElementById('a');
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize + 10) + 'px';
 }
 
